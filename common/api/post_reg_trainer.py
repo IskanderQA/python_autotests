a9393a9c9328d11f66a701e3aaac757791c7bd41
i.fayzrakhmanov <biketrial666@gmail.com>
2023-09-15 16:05:34 +0300

    some changes

--- common/api/post_reg_trainer.py
+++ common/api/post_reg_trainer.py
@@ -3,6 +3,6 @@ from common.api.basic_methods import Api
 
 class CreateTrainerApi(Api):
 
-    def post_reg_trainer(self, headers={}, request_body={}):
+    def post_reg_trainer(self, request_body={}):
         url=f'{self.url}/trainers/reg'
         return self.post(url=url, headers=request_body, json_body=request_body)
